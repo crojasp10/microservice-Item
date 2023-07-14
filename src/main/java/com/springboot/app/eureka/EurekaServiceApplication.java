04e475613bc7b308ce0e016c709a217dbba2091d
CarlosRojas <crojasw100@gmail.com>
2023-07-14 18:51:31 -0500

    feature: Se agrega el microservico register

---
+++ src/main/java/com/springboot/app/eureka/EurekaServiceApplication.java
@@ -0,0 +1,17 @@
+package com.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaClient
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
